7cc0b0c66d74f8c507ea9edd4292115881589bdb
artofmayhem <chef_tony_miller@yahoo.com>
2021-06-20 11:35:16 -1000

    responsiveness fixes

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,12 +4,12 @@ const navMap = ["Home", "About", "Team", "Connect"]
 function Nav() {
             
     return (
-        <nav className={'flex flex-row justify-between bg-white h-8'}>
-                <h1 className={'text-black text-3xl m-4 p-2'}>SONVR DESIGN</h1>
-                <ul className={'flex flex-row '}>
+        <nav className={'flex flex-row flex-wrap justify-between bg-white h-8'}>
+                <h1 className={'text-black text-3xl m-2 p-2'}>SONVR DESIGN</h1>
+                <ul className={'flex flex-row  flex-wrap'}>
                        {navMap.map((item, idx) => {
                     return (
-                        <li className={'flex p-6 text-lg font-sans font-semibold'} key={idx}>{item}</li>
+                        <li className={'flex p-4 text-lg font-sans font-semibold'} key={idx}>{item}</li>
                     )
                 })}
                 </ul>
